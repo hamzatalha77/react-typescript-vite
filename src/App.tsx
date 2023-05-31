980a773f29d5e5209aaba2a7e4c103625ca685e2
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-31 13:38:30 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Routes } from 'react-router-dom'
 const App = () => {
-  return <div>App</div>
+  return <Routes></Routes>
 }
 
 export default App
