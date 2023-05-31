1c67b06f900a53026658dda9a6b4afef51f4bd88
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-31 14:06:13 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/new" element={<h1>New</h1>} />
-      <Route path="/:id">
-        <Route index element={<h1>Show</h1>} />
-        <Route path="edit" element={<h1>Edit</h1>} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   )
 }
 
