65561f8b975259281fc6c78dff093a06ae9472dc
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-01 13:34:27 +0100

    comit

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,9 +1,24 @@
-import { Form, Stack } from 'react-bootstrap'
+import { Form, Stack, Row, Col } from 'react-bootstrap'
 
 const NewNote = () => {
   return (
     <Form>
-      <Stack gap={4}> </Stack>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <Form.Select required />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Stack>
     </Form>
   )
 }
