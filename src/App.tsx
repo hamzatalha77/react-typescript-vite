cbae8055cf86a4952841e61b4d7a72ef8245e4c9
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-01 10:37:23 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom'
-
+import NewNote from './NewNote'
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/new" element={<NewNote />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
