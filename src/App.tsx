32a116933a8f960ca7a17966cba13743476e6318
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-01 13:44:05 +0100

    c

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,23 @@ import {
   Navigate,
 } from 'react-router-dom'
 import NewNote from './NewNote'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<NewNote />} />
-        <Route path="/:id">
-          <Route index element={<h1>Show</h1>} />
-          <Route path="edit" element={<h1>Edit</h1>} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <Container>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/new" element={<NewNote />} />
+          <Route path="/:id">
+            <Route index element={<h1>Show</h1>} />
+            <Route path="edit" element={<h1>Edit</h1>} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </Container>
   )
 }
 
