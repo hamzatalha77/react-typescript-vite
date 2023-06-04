c19d2df01fb520b7e464f206bb5eadd200ce7aa0
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-04 12:33:16 +0100

    co

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,5 +1,18 @@
+import { Col, Row, Stack } from 'react-bootstrap'
+
 const NoteList = () => {
-  return <></>
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Notes</h1>
+        </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal"></Stack>
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default NoteList
