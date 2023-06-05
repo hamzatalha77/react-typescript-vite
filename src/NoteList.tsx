68aece60fdae19a348b34ab056e31a3594695ed7
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-05 19:30:03 +0100

    c

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -24,6 +24,32 @@ const NoteList = () => {
             <Form.Control type="text" />
           </Form.Group>
         </Col>
+        <Col>
+          <Form.Group controlId="tags">
+            <Form.Label>Tags</Form.Label>
+            <CreatableReactSelect
+              onCreateOption={(label) => {
+                const newTag = { id: uuidV4(), label }
+                onAddTag(newTag)
+                setSelectedTags((prev) => [...prev, newTag])
+              }}
+              value={selectedTags.map((tag) => {
+                return { label: tag.label, value: tag.id }
+              })}
+              options={availableTags.map((tag) => {
+                return { label: tag.label, value: tag.id }
+              })}
+              onChange={(tags) => {
+                setSelectedTags(
+                  tags.map((tag) => {
+                    return { label: tag.label, id: tag.value }
+                  })
+                )
+              }}
+              isMulti
+            />
+          </Form.Group>
+        </Col>
       </Form>
     </>
   )
