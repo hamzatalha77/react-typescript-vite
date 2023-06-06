fa8404808bcadc89f3a9458e7bd0d0a39f215c4f
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-06 11:58:43 +0100

    commit

--- src/NoteCard.tsx
+++ src/NoteCard.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { SimplifiedNote } from './interface/FromInterface'
+import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const NoteCard = ({ id, title, tags }: SimplifiedNote) => {
-  return <div>NoteCard</div>
+  return (
+    <Card
+      as={Link}
+      to={`/${id}`}
+      className={`h-100 text-reset text-decoration-none ${styles.card}`}
+    >
+      <Card.Body></Card.Body>
+    </Card>
+  )
 }
 
 export default NoteCard
