a2d66262678d0b71456b282725b6a0eca070bdab
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-06 12:38:37 +0100

    commmit

--- src/NoteCard.tsx
+++ src/NoteCard.tsx
@@ -1,5 +1,5 @@
 import { SimplifiedNote } from './interface/FromInterface'
-import { Card } from 'react-bootstrap'
+import { Badge, Card, Stack } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import styles from './NoteList.module.css'
 
@@ -10,7 +10,27 @@ const NoteCard = ({ id, title, tags }: SimplifiedNote) => {
       to={`/${id}`}
       className={`h-100 text-reset text-decoration-none ${styles.card}`}
     >
-      <Card.Body></Card.Body>
+      <Card.Body>
+        <Stack
+          gap={2}
+          className="align-items-center justify-content-center h-100"
+        >
+          <span className="fs-5">{title}</span>
+          {tags.length > 0 && (
+            <Stack
+              gap={1}
+              direction="horizontal"
+              className="justify-content-center flex-wrap"
+            >
+              {tags.map((tag) => (
+                <Badge className="text-truncate" key={tag.id}>
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </Card.Body>
     </Card>
   )
 }
