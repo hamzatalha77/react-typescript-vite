27fa8502dbb9525e6e0dce4afdbf8d3443202bcd
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-06 12:24:33 +0100

    commit

--- src/NoteCard.tsx
+++ src/NoteCard.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import { SimplifiedNote } from './interface/FromInterface'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import styles from './NoteList.modules.css'
 
 const NoteCard = ({ id, title, tags }: SimplifiedNote) => {
   return (
