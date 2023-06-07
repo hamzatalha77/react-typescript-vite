f42f19f6964a015542310d0fbbaf05fb0cc8b4bc
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-07 19:07:37 +0100

    commit

--- src/NoteLayout.tsx
+++ src/NoteLayout.tsx
@@ -1,9 +1,11 @@
-import { useParams } from 'react-router-dom'
+import { Navigate, Outlet, useParams } from 'react-router-dom'
 import { NoteLyoutProps } from './interface/FromInterface'
 
 const NoteLayout = ({ notes }: NoteLyoutProps) => {
   const { id } = useParams()
   const note = notes.find((n) => n.id === id)
+  if (note == null) return <Navigate to="/" replace />
+  return <Outlet />
 }
 
 export default NoteLayout
