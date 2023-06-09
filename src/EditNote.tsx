eb84f0aedca0a3009b4d139139e519a792baffbc
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-09 19:08:32 +0100

    commiut

---
+++ src/EditNote.tsx
@@ -0,0 +1,22 @@
+import NewForm from './NewForm'
+import { NoteData, Tag } from './interface/FromInterface'
+
+export type NoteFormProps = {
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+const NewNote = ({ onSubmit, onAddTag, availableTags }: NoteFormProps) => {
+  return (
+    <div>
+      <h1 className="mb-4">New Note</h1>
+      <NewForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </div>
+  )
+}
+
+export default NewNote
